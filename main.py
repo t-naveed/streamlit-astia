b3dc074bab57bf03cc8eb90ad8bb3bb6f1a74be0
Naveed <tan7@njit.edu>
2022-10-16 21:31:38 -0400

    Error Handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 
 st.set_page_config(page_title="Astia a Smishing Platform", page_icon=":fishing_pole_and_fish:", layout="wide")
 
-
+global message
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -15,18 +15,15 @@ def local_css(file_name):
 local_css("styles/mystyle.css")
 
 
-def send_message():
-    account_sid = st.secrets["account_sid"]
-    auth_token = st.secrets["auth_token"]
+def send_message(str):
+    account_sid = "ACce40a630c9c1a52a972b68206fcbabf8"
+    auth_token = "aeed58674f95aec6cb81d87acab07b4f"
     client = Client(account_sid, auth_token)
     for i in range(len(number_column)):
         st.write(number_column[i])
         message = client.messages.create(
-            body="It is flu season, get your flu shot. If you are eligible for an updated COVID 19 booster, "
-                 "you can get your flu shot at the same time. Visit covid19.nj.gov/finder to find a convenient "
-                 "location near you or Call us: NJDOH COVID 19 Hotline, 855-568-0545. Para recibir este mensaje en "
-                 "espanol responda 1. Reply STOP to opt out",
-            from_=st.secrets["twilio_number"],
+            body=str,
+            from_= "+13029243686",
             to=number_column[i]
         )
         st.success(f"Sent to {i + 1} device")
@@ -45,48 +42,59 @@ with st.container():
                 ('Flu Season (Recommended)', 'Sick Family', 'Sign In', 'You Won', 'Double Tap'))
             if options == 'Flu Season (Recommended)':
                 st.write('You selected flu Season template. Hit Confirm and go to Target tab!')
+
+                message = """
+                It is flu season, get your flu shot. If you are eligible for an updated COVID 19 
+                booster,you can get your flu shot at the same time. Visit covid19.nj.gov/finder to find a convenient 
+                location near you or Call us: NJDOH COVID 19 Hotline, 855-568-0545. Para recibir este mensaje en 
+                espanol responda 1. Reply STOP to opt out
+                """
                 with right_column:
                     st.subheader("Preview")
                     st.image("images/flu.png", width=300)
                 with left_column:
-                    if st.button("Confirm Template", key=None, help=None, on_click=None):
-                        st.success("Template Selected. Go to Target tab")
+                    if st.button("See The SMS", key=None, help=None, on_click=None):
+                        st.success(message)
 
             if options == 'Sick Family':
                 st.write('You selected Sick Family template. Hit Confirm and go to Target tab!')
+                message = """Sick Family"""
                 with right_column:
                     st.subheader("Preview")
                     st.image("images/image1.png", width=300)
                     with left_column:
-                        if st.button("Confirm Template", key=None, help=None, on_click=None):
-                            st.success("Template Selected. Go to Target tab")
+                        if st.button("See The SMS", key=None, help=None, on_click=None):
+                            st.success(message)
 
             if options == 'Sign In':
                 st.write('You selected Sign In template. Hit Confirm and go to Target tab!')
+                message = """Sign In"""
                 with right_column:
                     st.subheader("Preview")
                     st.image("images/signin.jpg", width=300)
                     with left_column:
-                        if st.button("Confirm Template", key=None, help=None, on_click=None):
-                            st.success("Template Selected. Go to Target tab")
+                        if st.button("See The SMS", key=None, help=None, on_click=None):
+                            st.success(message)
 
             if options == 'You Won':
                 st.write('You selected You Won template. Hit Confirm and go to Target tab!')
+                message = """You won"""
                 with right_column:
                     st.subheader("Preview")
                     st.image("images/lottery.png", width=300)
                     with left_column:
-                        if st.button("Confirm Template", key=None, help=None, on_click=None):
-                            st.success("Template Selected. Go to Target tab")
+                        if st.button("See The SMS", key=None, help=None, on_click=None):
+                            st.success(message)
 
             if options == 'Double Tap':
                 st.write('You selected Double Tap template. Hit Confirm and go to Target tab!')
+                message = """Double Tap"""
                 with right_column:
                     st.subheader("Preview")
                     st.image("images/image2.png", width=300)
                     with left_column:
-                        if st.button("Confirm Template", key=None, help=None, on_click=None):
-                            st.success("Template Selected. Go to Target tab!")
+                        if st.button("See The SMS", key=None, help=None, on_click=None):
+                            st.success(message)
 
     with tab2:
         left_column, right_column = st.columns(2)
@@ -94,32 +102,33 @@ with st.container():
             st.subheader("Preview of Targeted Data")
         with left_column:
             st.subheader("Set Your Target")
-            uploaded_file = st.file_uploader("Select a CSV file contains employees name and phone number")
-            if uploaded_file is not None:
-                data = pd.read_csv(uploaded_file)
-                #data.to_html("Table.htm")
-                html_file = data.to_html()
-                try:
-                    number_column = data['PhoneNumber'].tolist()
-                    with right_column:
-                        st.write(html_file, unsafe_allow_html=True)
-                    with left_column:
-                        st.success(
-                            "Data Uploaded Successfully.\nHit the Launch Attack button. Go to campaign tab to see the "
-                            "result.")
-                        if st.button("Launch Attack", key=None, help=None, on_click=None):
-                            try:
-                                send_message()
-                            except:
-                                st.error("An exception occurred for this number")
-                except:
-                    st.error("Number Column's header should be \'PhoneNumber\'")
-
-            else:
-                st.warning("Type Sensetive: Must upload a CSV")
-            st.write("##")
-            st.write("##")
-
+            try:
+                uploaded_file = st.file_uploader("Select a CSV file contains employees name and phone number")
+                if uploaded_file is not None:
+                    data = pd.read_csv(uploaded_file)
+                        #data.to_html("Table.htm")
+                    html_file = data.to_html()
+                    try:
+                        number_column = data['PhoneNumber'].tolist()
+                        with right_column:
+                            st.write(html_file, unsafe_allow_html=True)
+                            with left_column:
+                                st.success(
+                                        "Data Uploaded Successfully.\nHit the Launch Attack button. Go to campaign tab to see the "
+                                        "result.")
+                                if st.button("Launch Attack", key=None, help=None, on_click=None):
+                                    try:
+                                        send_message(message)
+                                    except:
+                                        st.error("This Number is not verified. Twilio Trial Account Limitation!")
+                    except:
+                        st.error("Number Column's header should be \'PhoneNumber\'")
+                else:
+                    st.warning("Type Sensetive: Must upload a CSV")
+                st.write("##")
+                st.write("##")
+            except:
+                st.error("Type Sensetive: Must upload a CSV")
     with tab3:
         st.header("Dashboard")
         uploaded_file = st.file_uploader("Upload the file sent via email to see the analytics!")
